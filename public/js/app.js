3d86fd6c0bf8dca5deed2d0433fad5e4ac66bc85
Pitigoi <lascuzeanu_stefan@yahoo.com>
2025-12-22 18:20:08 +0000

    Test serve in pipeline

--- public/js/app.js
+++ public/js/app.js
@@ -31832,8 +31832,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/anamus/Projects/laracon-2019/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/anamus/Projects/laracon-2019/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /workspaces/vulnerable-laravel-app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /workspaces/vulnerable-laravel-app/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
